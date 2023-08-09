062327872da7d0ab973610a3aba0942422a75a55
Tiago Angelo <tiago.angelo@totvs.com.br>
2023-08-09 10:34:18 -0300

    Add hashicorp/time

--- providers.tf
+++ providers.tf
@@ -4,6 +4,7 @@ terraform {
     kubernetes = { source = "hashicorp/kubernetes", version = ">= 2.22.0, < 4.0.0" }
     helm       = { source = "hashicorp/helm", version = ">= 2.10.1, < 3.0.0" }
     kubectl    = { source = "alekc/kubectl", version = ">= 2.0.2, < 3.0.0" }
+    time       = { source = "hashicorp/time", version = ">= 0.9.1, < 1.0.0" }
   }
   required_version = ">= 1.0"
 }
